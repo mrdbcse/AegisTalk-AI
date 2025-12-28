6e19257566da8ee5a7af3d691afa4ff80451eeed
Debjyoti Banerjee <mr.db.cse@gmail.com>
2025-12-28 21:51:37 +0530

    UI setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+const App = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+        repellendus ducimus tempora atque delectus aperiam ad molestias
+        consectetur porro accusamus. Magni nulla ad itaque obcaecati accusantium
+        eos numquam a quod.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+        dolores laboriosam. Dolorum dolores debitis odio mollitia perspiciatis
+        facilis dolore eius harum repellat amet! Blanditiis vitae molestias
+        temporibus tempore voluptas natus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi neque
+        voluptates quam deleniti ullam repellendus ab fugiat libero, doloremque
+        assumenda architecto sapiente labore magni consequuntur delectus tenetur
+        asperiores quo tempora!
+      </p>
+    </div>
+  );
+};
+
+export default App;
